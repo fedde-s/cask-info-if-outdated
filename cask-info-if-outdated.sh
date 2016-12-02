9a7f9830779a206f14895cf50dd2165441e28632
Fedde Schaeffer <fedde.schaeffer@gmail.com>
2016-12-02 15:20:00 +0100

    Make the script select from installed casks
    This is what I wrote the script for, but the first version required
    the list of cask names to be specified on stdin.

--- cask-info-if-outdated.sh
+++ cask-info-if-outdated.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-stanza=`brew cask info "$1"`
-version=`echo "$stanza" | head -1 | sed -e "s/.*: //"`
-if ! echo "$stanza" | grep Caskroom | grep "$version" >/dev/null; then
+function check_stanza () {
+  stanza=`brew cask info "$1"`
+  version=`echo "$stanza" | head -1 | sed -e "s/.*: //"`
+  if ! echo "$stanza" | grep Caskroom | grep "$version" >/dev/null; then
     echo "$stanza"
-fi
+  fi
+}
+
+export -f check_stanza
+brew cask list | xargs -n 1 -- sh -c 'check_stanza "$@"' _
