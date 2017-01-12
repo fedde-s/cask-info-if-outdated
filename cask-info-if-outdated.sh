868ba0042c7cd05cf9e34fe48b777df2bcd2f89e
Fedde Schaeffer <fedde.schaeffer@gmail.com>
2017-01-12 17:01:00 +0100

    Show progress while handling non-outdated casks
    Since brew cask takes a while to collect the information for casks,
    print a dot after handling each cask that does not cause other
    output to show that the script is actually doing something in those
    seconds.

--- cask-info-if-outdated.sh
+++ cask-info-if-outdated.sh
@@ -4,9 +4,13 @@ function check_stanza () {
   stanza=`brew cask info "$1"`
   version=`echo "$stanza" | head -1 | sed -e "s/.*: //"`
   if ! echo "$stanza" | grep Caskroom | grep "$version" >/dev/null; then
+    echo
     echo "$stanza"
+  else
+    printf %c .
   fi
 }
 
 export -f check_stanza
 brew cask list | xargs -n 1 -- sh -c 'check_stanza "$@"' _
+echo
