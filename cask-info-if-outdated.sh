921dcfe2b6c0627618d8cfb3b990a76ada58643d
Fedde Schaeffer <fedde.schaeffer@gmail.com>
2017-01-30 18:30:00 +0100

    Ensure that version tags are matched completely
    Occasionally, the name/number of the current version is a substring
    of a previous version number, e.g. with the word 'beta' removed.
    This will make the script recognise these as different versions.

--- cask-info-if-outdated.sh
+++ cask-info-if-outdated.sh
@@ -3,7 +3,7 @@
 function check_stanza () {
   stanza=`brew cask info "$1"`
   version=`echo "$stanza" | head -1 | sed -e "s/.*: //"`
-  if ! echo "$stanza" | grep Caskroom | grep "$version" >/dev/null; then
+  if ! echo "$stanza" | grep Caskroom | grep "/$1/$version[/ ]" >/dev/null; then
     echo
     echo "$stanza"
   else
